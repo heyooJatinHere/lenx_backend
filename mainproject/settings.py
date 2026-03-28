2b7f6cdfd986697eff4602255a39f0b9a76099e7
ixambeejatin <jatin.kumar@ixambee.com>
2026-03-28 23:34:41 +0530

    fixed

--- mainproject/settings.py
+++ mainproject/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'lenx_backend.urls'
+ROOT_URLCONF = 'mainproject.urls'
 
 TEMPLATES = [
     {
@@ -66,7 +66,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'lenx_backend.wsgi.application'
+WSGI_APPLICATION = 'mainproject.wsgi.application'
 
 
 # Database
